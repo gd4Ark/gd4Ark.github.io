e8f872b4084376785c05c53d2900c0acf0edbdd6
蔡红辉 <gd.4ark@gmail.com>
2022-02-17 13:20:23 +0800

    feat: 首页显示 weekly

--- themes/cactus/scripts/custom-index.js
+++ themes/cactus/scripts/custom-index.js
@@ -1,29 +1,36 @@
-var pagination = require('hexo-pagination');
+var pagination = require('hexo-pagination')
 
-hexo.extend.generator.register('customIndex', function(locals){
-  locals.posts.forEach(function(post) {
+hexo.extend.generator.register('customIndex', function (locals) {
+  locals.posts.forEach(function (post) {
     //  set post default type as post, 设置默认值
     if (post.type == undefined) {
-      post.type = "post"
+      post.type = 'post'
     }
   })
 
-  const config = this.config;
-  const posts = locals.posts.sort(config.index_generator.order_by);
+  const config = this.config
+  const posts = locals.posts.sort(config.index_generator.order_by)
   // filter posts with type == "post", 过滤
-  const postsWithoutWeekly = posts.find({ "type": "post" })
+  const postsWithoutWeekly = posts.find({ type: 'post' })
 
-  posts.data.sort((a, b) => (b.sticky || 0) - (a.sticky || 0));
+  posts.data.sort((a, b) => (b.sticky || 0) - (a.sticky || 0))
 
-  const paginationDir = config.pagination_dir || 'page';
-  const path = config.index_generator.path || '';
+  const paginationDir = config.pagination_dir || 'page'
+  const path = config.index_generator.path || ''
+
+  const weeklyTag = config.weekly_generator?.tag || 'weekly'
+  const weeklyPosts = locals.tags
+    .findOne({ name: weeklyTag })
+    .posts.sort('date', 'desc')
+    .limit(1)
 
   return pagination(path, postsWithoutWeekly, {
     perPage: config.index_generator.per_page,
     layout: ['index', 'archive'],
     format: paginationDir + '/%d/',
     data: {
-      __index: true
+      __index: true,
+      weeklyPosts: weeklyPosts
     }
-  });
-});
+  })
+})
